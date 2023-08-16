9f8524ba07997f9fa2d982758405e5206930eed3
Jaykm00 <36683739+theblackpanther00@users.noreply.github.com>
2023-08-16 16:21:47 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,38 +1,48 @@
-// Get references to HTML elements
 const taskInput = document.getElementById('taskInput');
 const addButton = document.getElementById('addButton');
 const taskList = document.getElementById('taskList');
 
-function addTask(){
+// Retrieve tasks from local storage if available
+let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
-  const taskText = taskInput.value;
-  
-  if(taskText.trim() !== ''){
-
+// Render tasks from local storage
+function renderTasks() {
+  taskList.innerHTML = '';
+  tasks.forEach(function(taskText) {
     const taskItem = document.createElement('li');
     taskItem.textContent = taskText;
 
-    const deleteButton = document.createElement('button')
+    const deleteButton = document.createElement('button');
     deleteButton.textContent = '-';
-
     deleteButton.addEventListener('click', function () {
-      // Remove the task item from the task list
+      // Remove the task item from the task list and update local storage
+      const taskIndex = tasks.indexOf(taskText);
+      if (taskIndex > -1) {
+        tasks.splice(taskIndex, 1);
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+      }
       taskList.removeChild(taskItem);
     });
     taskItem.appendChild(deleteButton);
 
     taskList.appendChild(taskItem);
+  });
+}
 
-    //clears input field
+function addTask(){
+  const taskText = taskInput.value;
 
-
-    taskInput.value = '';
+  if(taskText.trim() !== ''){
+    tasks.push(taskText); // Add task to the tasks array
+    localStorage.setItem('tasks', JSON.stringify(tasks)); // Update local storage
+    renderTasks(); // Render tasks
+    taskInput.value = ''; // Clear input field
   }
 }
 
 addButton.addEventListener('click', addTask);
 
-//displays the date
+// Display the date
 const todayDiv = document.getElementById('today');
 const currentDate = new Date();
 const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
@@ -40,11 +50,10 @@ const formattedDate = currentDate.toLocaleDateString(undefined, options);
 
 todayDiv.textContent = formattedDate;
 
-
-//displays quotes
+// Display quotes
 const quote = document.getElementById('quote');
 const btn = document.getElementById('btn');
-const quotes = ["1% Better Every Day", "Put God First", "Stick to the plan."];
+const quotes = ["1% Better Every Day", "Put God First", "Stick to the plan.", "Life gets hard sometimes, Just rely on God and keep going." ,"Every day is a new chance to make a difference.", "Believe in yourself and you will be unstoppable.", "The harder you work, the luckier you get."];
 
 function getQuote() {
   const randomIndex = Math.floor(Math.random() * quotes.length);
@@ -53,3 +62,6 @@ function getQuote() {
 } 
 
 btn.addEventListener('click', getQuote);
+
+// Render tasks on page load
+renderTasks();
